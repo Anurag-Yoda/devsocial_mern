2f179cd6622dc107de0f0a27df88195269372f46
Anurag-Yoda <anurag.404@outlook.com>
2020-05-07 18:51:15 +0530

    complete till user registration

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,25 +1,53 @@
 const express = require("express");
 const router = express.Router();
 const User = require("../../models/User");
-const {Check, validationResult} = require("express-validator");  
+const gravatar = require("gravatar");
+const bcrypt = require('bcryptjs');
+const { check, validationResult } = require("express-validator");
 
 // register user route
-router.post("/",[Check('name').not().isEmpty(), Check('email').isEmail(), Check('password').isLength({min:6})], async (req, res, next) => {
+router.post(
+  "/",
+  [
+    check("name").not().isEmpty(),
+    check("email").isEmail(),
+    check("password").isLength({ min: 6 }),
+  ],
+  async (req, res, next) => {
     const errors = validationResult(req);
-   if(!errors.isEmpty()){
-       return res.status(400).json({errors: errors.array()});
-   }
-  let userCreated;
-  console.log(req.body);
-  try {
-    userCreated = await new User({
-      name: req.body.name,
-    });
-  } catch (error) {
-    console.log(error);
-    return next(error);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    const { name, email, password } = req.body;
+    let user;
+    try {
+       user = await User.findOne({ email: email });
+      if (user) {
+        res.status(400).json({ errors: [{ msg: "User already exists" }] });
+      }
+
+      const avatar = gravatar.url(email, {
+        s: "200",
+        r: "pg",
+        d: "mm",
+      });
+
+      user = new User({
+        name: name,
+        email:email,
+        password:password,
+        avatar:avatar
+      });
+      
+      const salt = await bcrypt.genSalt(10);
+      user.password = await bcrypt.hash(password, salt);
+      
+      await user.save();
+      res.status(200).send('user registered successfuly')
+    } catch (error) {
+        return next(error);
+    }
   }
-  res.status(200).json(userCreated);
-});
+);
 
 module.exports = router;
