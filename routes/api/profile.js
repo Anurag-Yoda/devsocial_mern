c5a4c6333293baf04285007bd107e1b7946bcd28
Anurag-Yoda <anurag.404@outlook.com>
2020-05-07 23:05:13 +0530

    added single user profile route

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,8 +1,23 @@
 const express = require("express");
 const router = express.Router();
+const auth = require("../../middleware/auth");
+const Profile = require('../../models/Profile');
+const User = require('../../models/User');
 
-router.get("/", (req, res, next) => {
-  res.send("profile router");
+
+
+//route for single user profile
+router.get("/me",auth, async (req, res, next) => {
+ try {
+     const profile = await (await Profile.findOne({user: req.user.id})).populated('user',['name','avatar']);
+     if(!profile){
+         res.status(404).json({msg:'No profile found'});
+     }
+     res.status(200).json(profile);
+ } catch (error) {
+     res.status(500).json({msg:"server error"});
+     return next(error)
+ }
 });
 
 module.exports = router;
