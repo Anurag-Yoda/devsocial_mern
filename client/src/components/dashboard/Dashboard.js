8785206356f7fe4194deb322e57c141eeffe75fe
Anurag-Yoda <anurag.404@outlook.com>
2020-05-11 00:03:31 +0530

    delete profile,education & exp

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -1,10 +1,12 @@
 import React, { useEffect, Fragment } from "react";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
-import { getCurrentProfile } from "../../actions/profile";
+import { getCurrentProfile, deleteAccount } from "../../actions/profile";
 import Spinner from "../layout/Spinner";
 import { Link } from "react-router-dom";
 import DashboardActions from "./DashboardActions";
+import Experience from "./Experience";
+import Education from "./Education";
 
 const Dashboard = ({
   getCurrentProfile,
@@ -26,12 +28,24 @@ const Dashboard = ({
         <i className="fas fa-user"></i> Welcome {user && user.name}
       </p>
       {profile !== null ? (
-        <Fragment> <DashboardActions/></Fragment>
+        <Fragment>
+          
+          <DashboardActions />
+            <Experience experience = {profile.experience} />
+            <Education education = {profile.education} />
+            <div className = 'my-2'>
+              <button className = 'btn btn-danger' onClick = {() => deleteAccount()}>
+                <i className = 'fas fa-user-minus'></i>Delete My Account
+              </button>
+            </div>
+        </Fragment>
       ) : (
-        <Fragment> You have not created the profile , Create Now!
-        <Link to = '/create-profile' className = 'btn btn-primary my-1'>
-          Create Profile
-        </Link>
+        <Fragment>
+       
+          You have not created the profile , Create Now!
+          <Link to="/create-profile" className="btn btn-primary my-1">
+            Create Profile
+          </Link>
         </Fragment>
       )}
     </Fragment>
@@ -40,6 +54,7 @@ const Dashboard = ({
 
 Dashboard.propTypes = {
   getCurrentProfile: PropTypes.func.isRequired,
+  deleteAccount: PropTypes.func.isRequired,
   auth: PropTypes.object.isRequired,
   profile: PropTypes.object.isRequired,
 };
@@ -49,4 +64,4 @@ const mapStateToProps = (state) => ({
   profile: state.profile,
 });
 
-export default connect(mapStateToProps, { getCurrentProfile })(Dashboard);
+export default connect(mapStateToProps, { getCurrentProfile , deleteAccount })(Dashboard);
--- routes/api/profile.js
+++ routes/api/profile.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const auth = require("../../middleware/auth");
 const Profile = require("../../models/Profile");
 const User = require("../../models/User");
+const Post = require("../../models/Post");
 const { check, validationResult } = require("express-validator");
 
 //route for single user profile
@@ -136,6 +137,12 @@ router.get("/user/:user_id", async (req, fres, next) => {
 
 router.delete("/", auth, async (req, res, next) => {
   try {
+
+    //remove user post 
+
+
+    await Post.deleteMany({user: req.user.id});
+
     // remove profile code
     await Profile.findOneAndRemove({ user: req.user.id });
 
